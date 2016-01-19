1befd2456eb2aee81e13183f395f72bbab2d6b44
Manuel Kaufmann <humitos@gmail.com>
2016-01-18 22:00:34 -0500

    Create HTML file using pandoc-bootstrap-template

--- bin/create_html.sh
+++ bin/create_html.sh
@@ -2,5 +2,7 @@
 
 pandoc -s --from markdown \
   -o django-coach-spanish.html \
+  --template pandoc-bootstrap-template/template.html \
+  --css pandoc-bootstrap-template/template.css \
   traducidos/README.md traducidos/intro/README.md \
   traducidos/tips/README.md traducidos/summary/README.md
